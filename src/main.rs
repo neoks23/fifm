b5506cf0baeb4de44fe0648614cd8f80c3fd49a2
koen <neoks23@gmail.com>
2022-06-28 21:03:47 +0200

    current directory

--- src/main.rs
+++ src/main.rs
@@ -83,16 +83,20 @@ struct App{
     messages: Vec<String>,
 
     items: StatefulList<String>,
+
+    title: String
 }
 
 impl Default for App {
     fn default() -> App {
         let cd_items = list_current_dir();
+        let title = get_current_dir();
         App {
             input: String::new(),
             input_mode: InputMode::Normal,
             messages: Vec::new(),
-            items: StatefulList::with_items(cd_items)
+            items: StatefulList::with_items(cd_items),
+            title
         }
     }
 }
@@ -109,6 +113,14 @@ fn list_current_dir() -> Vec<String>{
     let mut cd_items: Vec<String> = stdout.split(" ").map(String::from).collect();
     cd_items.pop();
     cd_items
+}
+fn get_current_dir() -> String{
+    let mut output = Command::new("pwd")
+        .output()
+        .expect("ls cmd failed to start");
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let mut cd: String = stdout.to_string();
+    cd
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -148,7 +160,7 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                 InputMode::Normal => match key.code {
                     KeyCode::Char('e') => app.input_mode = InputMode::Editing,
                     KeyCode::Char('q') => return Ok(()),
-                    KeyCode::Left => app.items.unselect(),
+                    KeyCode::Left | KeyCode::Right => app.items.unselect(),
                     KeyCode::Down => app.items.next(),
                     KeyCode::Up => app.items.previous(),
                     _ => {}
@@ -182,96 +194,25 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let items: Vec<ListItem> =
         app.items.items.iter().map(|i| {
             let mut lines = vec![Spans::from(i.to_string())];
-            ListItem::new(lines).style(Style::default().fg(Color::Black).bg(Color::White))
+            ListItem::new(lines).style(Style::default().fg(Color::LightCyan))
         })
         .collect();
 
     let items = List::new(items)
-        .block(Block::default().borders(Borders::ALL).title("List"))
+        .block(
+            Block::default()
+                .border_style(
+                    Style::default().fg(Color::Black)
+                    .add_modifier(Modifier::BOLD)
+                )
+            .borders(Borders::ALL).title(app.title.to_string())
+        )
         .highlight_style(
             Style::default()
-                .bg(Color::LightGreen)
+                .bg(Color::Black)
                 .add_modifier(Modifier::BOLD),
         )
         .highlight_symbol(">> ");
 
     f.render_stateful_widget(items, chunks[0], &mut app.items.state);
-    /*
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(2)
-        .constraints(
-            [
-                Constraint::Length(1),
-                Constraint::Length(3),
-                Constraint::Min(1),
-            ]
-            .as_ref(),
-
-        )
-        .split(f.size());
-
-
-    let (msg, style) = match app.input_mode {
-        InputMode::Normal => (
-            vec![
-                Span::raw("Press "),
-                Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to exit, "),
-                Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to start editing, "),
-                Span::styled("l", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to list contents. ")
-            ],
-            Style::default().add_modifier(Modifier::RAPID_BLINK),
-        ),
-        InputMode::Editing => (
-            vec![
-                Span::raw("Press "),
-                Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to stop editing, "),
-                Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to record the message"),
-            ],
-            Style::default()
-        ),
-    };
-
-    let mut text = Text::from(Spans::from(msg));
-    text.patch_style(style);
-    let help_message = Paragraph::new(text);
-    f.render_widget(help_message, chunks[0]);
-
-    let input = Paragraph::new(app.input.as_ref())
-        .style(match app.input_mode {
-            InputMode::Normal => Style::default(),
-            InputMode::Editing => Style::default().fg(Color::Yellow),
-        })
-        .block(Block::default().borders(Borders::ALL).title("Input"));
-    f.render_widget(input, chunks[1]);
-
-    match app.input_mode {
-        InputMode::Normal =>
-            //Hide cursor
-            {}
-        InputMode::Editing => {
-            f.set_cursor(
-                chunks[1].x + app.input.width() as u16 + 1,
-                chunks[1].y + 1,
-            )
-        }
-    }
-
-    let messages: Vec<ListItem> = app
-        .messages
-        .iter()
-        .enumerate()
-        .map(|(i, m)|{
-            let content = vec![Spans::from(Span::raw(format!("{}: {}",i,m)))];
-            ListItem::new(content)
-        })
-        .collect();
-    let messages =
-        List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
-    f.render_widget(messages, chunks[2]);*/
 }
